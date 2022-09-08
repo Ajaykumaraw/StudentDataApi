c2209eb0d2a1053ff1d585c89a183bd60e3fb7b9
ajaykumaraw <378ajaykumar@gmail.com>
2022-09-09 03:28:57 +0530

    http get request completed

---
+++ src/main/java/com/example/StudentRecords/Student/StudentConfig.java
@@ -0,0 +1,38 @@
+package com.example.StudentRecords.Student;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+                Student mariam = new Student(
+                        "mariam",
+                        "mariam@gmail.com",
+                       LocalDate.of(2000, Month.JANUARY,5),
+                         21
+                );
+
+                Student alex = new Student(
+                        "alex",
+                        "alex@gmail.com",
+                        LocalDate.of(20002, Month.JANUARY,5),
+                        21
+                );
+
+                repository.saveAll(
+                        List.of(mariam, alex));
+
+        };
+    }
+
+}
--- src/main/java/com/example/StudentRecords/Student/StudentControler.java
+++ src/main/java/com/example/StudentRecords/Student/StudentControler.java
@@ -1,12 +1,8 @@
 package com.example.StudentRecords.Student;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
@@ -14,22 +10,36 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:4200")
 public class StudentControler {
 
+        private final StudentService studentService;
+
+        @Autowired
+    public StudentControler(StudentService studentService) {
+        this.studentService = studentService;
+    }
 
     @GetMapping
     public List<Student> getStudents(){
-        return List.of(new Student(
-                1L,
-                "Ajay Kumar",
-                "ajaykumar@gmail.com",
-                LocalDate.of(2000, Month.JANUARY,5),
-                21
-        ),
-                new Student(
-                        2L,
-                        "pushpa",
-                        "jiya@gmail.com",
-                        LocalDate.of(2002, Month.JANUARY,5),
-                        20
-                ));
+        return studentService.getStudents();
     }
+
+    @PostMapping
+    public void registerStudent(@RequestBody Student student){
+            studentService.addNewStudent(student);
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
